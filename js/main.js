6ebd91e9d652d8b767e5feacfb7c12dcb31e7134
Moria Hamami <moriahamami@gmail.com>
2022-12-05 21:14:31 +0200

    Create contact form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('Starting up');
+'use strict'
+
+function submitForm(){
+    const $elMail = $('.email')
+    const $elSubj = $('.subject')
+    const $elMsg = $('.msg')
+    const body = $elMsg.val() + '. Contact me at: ' + $elMail.val() 
+
+    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=moriahamami@gmail.com&su=${$elSubj.val()}&body=${body}`)
+
+    $elMail.val('')
+    $elMsg.val('')
+    $elSubj.val('')
+}
